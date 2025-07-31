29962ec74efaec6ca1e4ef1d434ad9086d0fa140
ikadar <odesk.kadari@gmail.com>
2025-07-31 20:15:50 +0200

    Several improvements and code cleaning

--- src/Controller/DisplayController.php
+++ src/Controller/DisplayController.php
@@ -2,31 +2,15 @@
 
 namespace App\Controller;
 
-use App\Domain\Action\AbstractAction;
-use App\Domain\Action\Action;
-use App\Domain\Action\ActionPathNode;
-use App\Domain\Action\ActionTreeNode;
-use App\Domain\Action\ActionType;
-use App\Domain\Action\Interfaces\ActionInterface;
-use App\Domain\Action\Interfaces\ActionTreeInterface;
-use App\Domain\Action\Interfaces\ActionTreeNodeInterface;
-use App\Domain\Equipment\Interfaces\EquipmentFactoryInterface;
-use App\Domain\Equipment\Interfaces\EquipmentServiceInterface;
 use App\Domain\Equipment\Interfaces\MachineInterface;
-use App\Domain\Equipment\MachineType;
-use App\Domain\Geometry\Dimensions;
 use App\Domain\Geometry\Interfaces\RectangleInterface;
-use App\Domain\Layout\Calculator;
-use App\Domain\Layout\Interfaces\GridFittingInterface;
 use App\Domain\Sheet\Interfaces\InputSheetInterface;
-use App\Domain\Sheet\Interfaces\PressSheetInterface;
-use App\Domain\Sheet\PrintFactory;
+use App\Entity\ActionPath;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Uid\Uuid;
 
 class DisplayController extends AbstractController
 {
@@ -39,6 +23,7 @@ class DisplayController extends AbstractController
 
     public function __construct(
         protected KernelInterface $kernel,
+        protected EntityManagerInterface $em,
     )
     {
     }
@@ -50,29 +35,17 @@ class DisplayController extends AbstractController
         $impositionId,
     ): JsonResponse
     {
-        $data = $this->loadData($jobId, $partId, $impositionId);
+        $data = $this->loadData($impositionId);
 
         return new JsonResponse(
             $data,
             JsonResponse::HTTP_OK
         );
-
-
     }
 
-    public function loadData($jobId, $partId, $impositionId): ?array
+    public function loadData($impositionId): ?array
     {
-        $path = sprintf("%s/data/%s/parts/%s.json", $this->kernel->getProjectDir(), $jobId, $partId);
-
-        $jsonData = file_get_contents($path);
-        $data = json_decode($jsonData, true);
-
-        foreach ($data as $item) {
-            if ($item['id'] === $impositionId) {
-                return $item;
-            }
-        }
-        return null;
+        return $this->em->getRepository(ActionPath::class)->find($impositionId)->getJson();
     }
 
 }
